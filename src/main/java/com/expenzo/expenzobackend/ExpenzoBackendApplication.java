ce08f099539e0c287853aea04e63be7d05e8206f
Hamid <hamid70yazdi@gmail.com>
2025-07-13 02:40:06 +0200

    start

---
+++ src/main/java/com/expenzo/expenzobackend/ExpenzoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.expenzo.expenzobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenzoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenzoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/expenzo/expenzobackend/ExpenzoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expenzo.expenzobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenzoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
